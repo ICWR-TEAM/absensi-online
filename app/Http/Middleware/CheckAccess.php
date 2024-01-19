7e890a233a0ec092037f2d611268a39ddcc49040
Jon3sjns <silendprojeck@gmail.com>
2024-01-19 17:26:28 +0700

    updated

--- app/Http/Middleware/CheckAccess.php
+++ app/Http/Middleware/CheckAccess.php
@@ -18,24 +18,6 @@ class CheckAccess
     public function handle($request, Closure $next)
     {
         $db = DB::table("setting_absensi")->where("id", 1)->first();
-        // $waktu_awal = now();
-        //
-        // if($db->tutup_otomatis === "iya"){
-        //     $pisah_tutup = explode(":", $db->waktu_tutup);
-        //     $waktu_tutup = now()->setTime($pisah_tutup[0], $pisah_tutup[1], 0);
-        //     if ($waktu_awal > $waktu_tutup) {
-        //         return redirect()->intended("user/waktu_tutup")->with("error_tutup","waktu ditutup");
-        //     }
-        // }
-        //
-        // if($db->buka_otomatis === "iya"){
-        //     $pisah_buka = explode(":", $db->waktu_buka);
-        //     $akses_waktu_awal = now()->setTime($pisah_buka[0], $pisah_buka[1], 0);
-        //     if($waktu_awal < $akses_waktu_awal){
-        //         return redirect()->intended("user/waktu_buka")->with("error_buka", "waktu belum dibuka");
-        //     }
-        // }
-
         $pisah_waktu_tanggal = explode(" ", $db->updated_at);
         $pisah_tanggal = explode("-", $pisah_waktu_tanggal[0]);
         $pisah_waktu = explode(":", $pisah_waktu_tanggal[1]);
