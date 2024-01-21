0830ea1554fa11ecf99244df2989f9fade959e55
Jon3sjns <silendprojeck@gmail.com>
2024-01-21 09:35:58 +0700

    updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        if(env('FORCE_HTTPS',false)) { // Default value should be false for local server
+            URL::forceScheme('https');
+        }
     }
 }
